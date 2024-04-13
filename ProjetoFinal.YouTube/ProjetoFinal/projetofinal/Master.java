f7cafbce22e4af5de463465cf3bbed1409f690cc
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-04-13 13:25:44 -0300

    Update Projeto Final

--- ProjetoFinal.YouTube/ProjetoFinal/projetofinal/Master.java
+++ ProjetoFinal.YouTube/ProjetoFinal/projetofinal/Master.java
@@ -3,6 +3,7 @@ package projetofinal;
 public class Master {
 
 	public static void main(String[] args) {
+		
 		Video v[] = new Video[3];
 		v[0] = new Video("Aula 1 de POO");
 		v[1] = new Video("Aula 12 de PHP");
